82e807eea8f300078ae7809fad222c1de5a945ed
Red <47802951+redHild@users.noreply.github.com>
2020-02-06 13:01:34 -0500

    added heapSort.py

---
+++ heapSort.py
@@ -0,0 +1,29 @@
+from random import *
+from heapq import *
+
+def heapsort(L):
+    H = []
+    for x in L:
+        heappush(H, x)
+    for i in range(len(L)):
+        L[i] = heappop(H)
+
+# alternate version, a little faster.  The heapify function turns a list into
+# a heap faster than manually pushing the elements.
+def heapsort(L):
+    H = L[:]
+    heapify(H)
+    for i in range(len(L)):
+        L[i] = heappop(H)
+
+
+# testing code
+L = [4,6,14,9,7,2,11,5]
+print(L)
+heapsort(L)
+print(L)
+
+L = [randint(1,100) for i in range(100)]
+M = L[:]
+heapsort(L)
+print(L == sorted(M))
